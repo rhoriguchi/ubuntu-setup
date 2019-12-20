d0939d80c28d6ebf38a8c37e6f37b2fd0c554d2e
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2019-12-20 09:18:36 +0100

    Add gnome shell tweaks

--- init.sh
+++ init.sh
@@ -5,21 +5,19 @@
 # Mega
 # JDownloader
 
-######################### Gnome tweak #########################
-
-gsettings set org.gnome.desktop.interface gtk-enable-primary-paste false
-
 ######################### Apt packages #########################
 
 sudo apt update && sudo apt upgrade -y
 
 sudo apt install -y \
-  chrome \
+  dconf-editor \
   filezilla \
   firefox \
   git \
+  libreoffice \
   htop \
   k4dirstat \
+  make \
   maven \
   npm \
   openjdk-8-jdk \
@@ -208,6 +206,25 @@ wget -O /tmp/setup/shift.zip https://update.tryshift.com/download/version/4.0.2/
 unzip /tmp/setup/shift.zip -d /tmp/setup/shift
 sudo mv /tmp/setup/shift/shift-linux-v4.0.2 /usr/local/Shift
 /usr/local/Shift/Shift
+
+######################### Tweak GNOME Shell #########################
+
+gsettings set org.gnome.desktop.interface gtk-enable-primary-paste false
+gsettings set org.gnome.desktop.interface show-battery-percentage true
+gsettings set org.gnome.desktop.wm.preferences action-middle-click-titlebar 'none'
+gsettings set org.gnome.desktop.wm.preferences num-workspaces 1
+gsettings set org.gnome.login-screen enable-fingerprint-authentication false
+gsettings set org.gnome.login-screen enable-smartcard-authentication false
+gsettings set org.gnome.nautilus.preferences show-hidden-files true
+gsettings set org.gnome.shell favorite-apps "['firefox.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Terminal.desktop', 'spotify.desktop', 'shift-application.desktop', 'gitkraken.desktop', 'code.desktop']"
+gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'previews'
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'BOTTOM'
+gsettings set org.gnome.shell.extensions.dash-to-dock multi-monitor true
+gsettings set org.gnome.shell.extensions.dash-to-dock running-indicator-dominant-color true
+gsettings set org.gnome.shell.extensions.dash-to-dock show-apps-at-top true
+gsettings set org.gnome.shell.extensions.dash-to-dock show-mounts false
+gsettings set org.gnome.shell.extensions.desktop-icons show-home false
+gsettings set org.gnome.shell.extensions.desktop-icons show-trash false
 
 ######################### Cleanup manual downloads #########################
 
