7c100f83d7b53f7209bfa908b6c539c868310813
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2019-12-26 14:34:57 +0100

    Remove duplicate chrome

--- init.sh
+++ init.sh
@@ -110,12 +110,6 @@ echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sud
 curl -sS https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo apt update && sudo apt install google-chrome-stable -y
 
-######################### Chrome #########################
-
-echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
-curl -sS https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo apt update && sudo apt install google-chrome-stable -y
-
 ######################### Resilio Sync #########################
 
 echo "deb http://linux-packages.resilio.com/resilio-sync/deb resilio-sync non-free" | sudo tee /etc/apt/sources.list.d/resilio-sync.list
