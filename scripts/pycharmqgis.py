0dae6fa9fd65316529b8f3e478aae9f6f36e44cb
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2020-03-13 00:53:13 +0100

    Make python script more readable

--- scripts/pycharmqgis.py
+++ scripts/pycharmqgis.py
@@ -3,10 +3,12 @@ import os
 import sys
 
 paths = ':'.join(sys.path). \
-    replace('/home/{}'.format(getpass.getuser()), '~')
+    replace('/home/' + getpass.getuser(), '~')
 
 with open('pycharmqgis.sh', 'w') as fileHandler:
-    fileHandler.write('export PYTHONPATH=$PYTHONPATH:{}\n'.format(paths))
+    fileHandler.write('export PYTHONPATH=$PYTHONPATH:' + paths)
+    fileHandler.write('\n')
     fileHandler.write('sh ~/bin/pycharm')
+    fileHandler.write('\n')
 
 os._exit(0)
