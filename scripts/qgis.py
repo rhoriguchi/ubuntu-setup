e8f51becd648122fa3fc56a60fa5b7805b644ac8
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2020-03-12 21:02:14 +0100

    Remove /tmp/setup dir requirement for qgis installation

--- scripts/qgis.py
+++ scripts/qgis.py
@@ -2,12 +2,10 @@ import getpass
 import os
 import sys
 
-shell_script_path = '/tmp/setup/pycharmqgis.sh'
-
 paths = ':'.join(sys.path). \
     replace('/home/{}'.format(getpass.getuser()), '~')
 
-with open(shell_script_path, 'w') as fileHandler:
+with open('pycharmqgis.sh', 'w') as fileHandler:
     fileHandler.write('export PYTHONPATH=$PYTHONPATH:{}\n'.format(paths))
     fileHandler.write('sh ~/bin/pycharm')
 
