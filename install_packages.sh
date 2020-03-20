dffbd8c9d8d6769730eca493762c67061385a766
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2020-03-20 13:25:08 +0100

    Use curl for docker-compose since apt version is outdated

--- install_packages.sh
+++ install_packages.sh
@@ -95,8 +95,12 @@ sudo add-apt-repository "deb [arch=$(dpkg --print-architecture)] https://downloa
 sudo apt update && sudo apt install -y \
   docker-ce \
   docker-ce-cli \
-  docker-compose \
   containerd.io
+
+######################### Docker Compose #########################
+
+sudo curl -SL "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 ######################### Android development #########################
 
