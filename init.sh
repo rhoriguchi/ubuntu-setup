817e373f103dff98a6df7a34d5554c5cf8503ac6
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2020-04-14 13:39:57 +0200

    Add udev power management triggers

--- init.sh
+++ init.sh
@@ -3,3 +3,4 @@
 sh install_packages.sh
 sh init_dotfiles.sh
 sh tweak_gnome_settings.sh
+sh setup_power_management.sh
---
+++ setup_power_management.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -ex
+
+ac_command=(
+    "echo 3 > /sys/devices/platform/asus-nb-wmi/leds/asus::kbd_backlight/brightness &&"
+    "echo 24000 > /sys/class/backlight/intel_backlight/brightness"
+)
+battery_command=(
+    "echo 0 > /sys/devices/platform/asus-nb-wmi/leds/asus::kbd_backlight/brightness &&"
+    "echo 16000 > /sys/class/backlight/intel_backlight/brightness"
+)
+
+echo "ACTION==\"change\", SUBSYSTEM==\"power_supply\", KERNEL==\"AC0\", ATTR{online}==\"1\", RUN+=\"/bin/sh -c '${ac_command[@]}'\"
+ACTION==\"change\", SUBSYSTEM==\"power_supply\", KERNEL==\"AC0\", ATTR{online}==\"0\", RUN+=\"/bin/sh -c '${battery_command[@]}'\"" | sudo tee /etc/udev/rules.d/99-powertrigger.rules
+
+sudo udevadm control --reload-rules
