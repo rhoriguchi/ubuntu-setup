040309f7e4663dd70a6b48eba49379fc9dc85032
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2020-04-15 22:01:54 +0200

    Remove application from favorites

--- tweak_gnome_settings.sh
+++ tweak_gnome_settings.sh
@@ -19,7 +19,7 @@ gsettings set org.gnome.gedit.preferences.editor tabs-size 4
 gsettings set org.gnome.login-screen enable-fingerprint-authentication false
 gsettings set org.gnome.login-screen enable-smartcard-authentication false
 gsettings set org.gnome.nautilus.preferences show-hidden-files true
-gsettings set org.gnome.shell favorite-apps "['firefox.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Terminal.desktop', 'spotify.desktop', 'shift-application.desktop', 'gitkraken.desktop', 'code.desktop']"
+gsettings set org.gnome.shell favorite-apps "['firefox.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Terminal.desktop', 'shift-application.desktop', 'gitkraken.desktop', 'code.desktop']"
 gsettings set org.gnome.shell.extensions.dash-to-dock click-action "previews"
 gsettings set org.gnome.shell.extensions.dash-to-dock dock-position "BOTTOM"
 gsettings set org.gnome.shell.extensions.dash-to-dock multi-monitor true
