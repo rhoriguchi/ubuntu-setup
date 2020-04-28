6b83e573f8dd7c4ba6e9271713bab9000fc141ad
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2020-04-28 20:58:54 +0200

    Disable bluetooth on battery

--- set_up_power_management.sh
+++ set_up_power_management.sh
@@ -4,11 +4,13 @@ set -ex
 
 ac_command=(
     "echo 3 > /sys/devices/platform/asus-nb-wmi/leds/asus::kbd_backlight/brightness &&"
-    "echo 24000 > /sys/class/backlight/intel_backlight/brightness"
+    "echo 24000 > /sys/class/backlight/intel_backlight/brightness &&"
+    "service bluetooth start"
 )
 battery_command=(
     "echo 0 > /sys/devices/platform/asus-nb-wmi/leds/asus::kbd_backlight/brightness &&"
-    "echo 16000 > /sys/class/backlight/intel_backlight/brightness"
+    "echo 16000 > /sys/class/backlight/intel_backlight/brightness &&"
+    "service bluetooth stop"
 )
 
 echo "ACTION==\"change\", SUBSYSTEM==\"power_supply\", KERNEL==\"AC0\", ATTR{online}==\"1\", RUN+=\"/bin/sh -c '${ac_command[@]}'\"
