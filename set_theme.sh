09625a3a4ad46d0d4cd613e9b74a84d684dc0815
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2020-05-01 00:25:37 +0200

    Add color variables

--- set_theme.sh
+++ set_theme.sh
@@ -35,9 +35,26 @@ gsettings set org.gnome.desktop.interface icon-theme 'Papirus'
 
 ######################### Terminal Theme #########################
 
+black="rgb(0,0,0)"
+red="rgb(255,65,54)"
+green="rgb(46,204,64)"
+yellow="rgb(255,220,0)"
+blue="rgb(0,116,217)"
+magenta="rgb(177,13,201)"
+cyan="rgb(127,219,255)"
+white="rgb(255,255,255)"
+light_black="rgb(50,50,50)"
+light_red="rgb(255,141,134)"
+light_green="rgb(129,224,140)"
+light_yellow="rgb(255,234,102)"
+light_blue="rgb(102,171,232)"
+light_magenta="rgb(208,109,222)"
+light_cyan="rgb(178,233,255)"
+light_white="rgb(255,255,255)"
+
 dconf write /org/gnome/terminal/legacy/profiles:/$(dconf list /org/gnome/terminal/legacy/profiles:/)background-color "'rgb(48,48,48)'"
-dconf write /org/gnome/terminal/legacy/profiles:/$(dconf list /org/gnome/terminal/legacy/profiles:/)cursor-background-color "'rgb(177,13,201)'"
+dconf write /org/gnome/terminal/legacy/profiles:/$(dconf list /org/gnome/terminal/legacy/profiles:/)cursor-background-color "'$magenta'"
 dconf write /org/gnome/terminal/legacy/profiles:/$(dconf list /org/gnome/terminal/legacy/profiles:/)cursor-colors-set true
-dconf write /org/gnome/terminal/legacy/profiles:/$(dconf list /org/gnome/terminal/legacy/profiles:/)cursor-foreground-color "'rgb(255,255,255)'"
-dconf write /org/gnome/terminal/legacy/profiles:/$(dconf list /org/gnome/terminal/legacy/profiles:/)foreground-color "'rgb(255,255,255)'"
-dconf write /org/gnome/terminal/legacy/profiles:/$(dconf list /org/gnome/terminal/legacy/profiles:/)palette "['rgb(0,0,0)', 'rgb(255,65,54)', 'rgb(46,204,64)', 'rgb(255,220,0)', 'rgb(0,116,217)', 'rgb(177,13,201)', 'rgb(127,219,255)', 'rgb(255,255,255)', 'rgb(50,50,50)', 'rgb(255,141,134)', 'rgb(129,224,140)', 'rgb(255,234,102)', 'rgb(102,171,232)', 'rgb(208,109,222)', 'rgb(178,233,255)', 'rgb(255,255,255)']"
+dconf write /org/gnome/terminal/legacy/profiles:/$(dconf list /org/gnome/terminal/legacy/profiles:/)cursor-foreground-color "'$white'"
+dconf write /org/gnome/terminal/legacy/profiles:/$(dconf list /org/gnome/terminal/legacy/profiles:/)foreground-color "'$white'"
+dconf write /org/gnome/terminal/legacy/profiles:/$(dconf list /org/gnome/terminal/legacy/profiles:/)palette "['$black', '$red', '$green', '$yellow', '$blue', '$magenta', '$cyan', '$white', '$light_black', '$light_red', '$light_green', '$light_yellow', '$light_blue', '$light_magenta', '$light_cyan', '$light_white']"
