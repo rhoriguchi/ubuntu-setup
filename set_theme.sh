398440555b0e021cb4a23f21dabf8f519a7d4fca
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2020-04-30 23:38:28 +0200

    Set custom shell colors

--- set_theme.sh
+++ set_theme.sh
@@ -37,4 +37,8 @@ gsettings set org.gnome.desktop.interface icon-theme 'Papirus'
 ######################### Terminal Theme #########################
 
 dconf write /org/gnome/terminal/legacy/profiles:/$(dconf list /org/gnome/terminal/legacy/profiles:/)background-color "'rgb(48,48,48)'"
+dconf write /org/gnome/terminal/legacy/profiles:/$(dconf list /org/gnome/terminal/legacy/profiles:/)cursor-background-color "'rgb(177,13,201)'"
+dconf write /org/gnome/terminal/legacy/profiles:/$(dconf list /org/gnome/terminal/legacy/profiles:/)cursor-colors-set true
+dconf write /org/gnome/terminal/legacy/profiles:/$(dconf list /org/gnome/terminal/legacy/profiles:/)cursor-foreground-color "'rgb(255,255,255)'"
 dconf write /org/gnome/terminal/legacy/profiles:/$(dconf list /org/gnome/terminal/legacy/profiles:/)foreground-color "'rgb(255,255,255)'"
+dconf write /org/gnome/terminal/legacy/profiles:/$(dconf list /org/gnome/terminal/legacy/profiles:/)palette "['rgb(0,0,0)', 'rgb(255,65,54)', 'rgb(46,204,64)', 'rgb(255,220,0)', 'rgb(0,116,217)', 'rgb(177,13,201)', 'rgb(127,219,255)', 'rgb(255,255,255)', 'rgb(50,50,50)', 'rgb(255,141,134)', 'rgb(129,224,140)', 'rgb(255,234,102)', 'rgb(102,171,232)', 'rgb(208,109,222)', 'rgb(178,233,255)', 'rgb(255,255,255)']"
