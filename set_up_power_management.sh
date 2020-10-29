a6ace55d0cc4165eea45fb363de25d1b587894e6
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2020-10-29 14:09:08 +0100

    Some minor improvements

--- set_up_power_management.sh
+++ set_up_power_management.sh
@@ -2,18 +2,20 @@
 
 set -ex
 
-ac_command=(
-    "echo 3 > /sys/devices/platform/asus-nb-wmi/leds/asus::kbd_backlight/brightness &&"
+ac_commands=(
+    "echo 3 > /sys/devices/platform/asus-nb-wmi/leds/asus::kbd_backlight/brightness"
+    "&&"
     "echo 24000 > /sys/class/backlight/intel_backlight/brightness"
 )
-battery_command=(
-    "echo 0 > /sys/devices/platform/asus-nb-wmi/leds/asus::kbd_backlight/brightness &&"
+battery_commands=(
+    "echo 0 > /sys/devices/platform/asus-nb-wmi/leds/asus::kbd_backlight/brightness"
+    "&&"
     "echo 16000 > /sys/class/backlight/intel_backlight/brightness"
 )
 
 sudo dd status=none of=/etc/udev/rules.d/99-powertrigger.rules << EOL
-ACTION=="change", SUBSYSTEM=="power_supply", KERNEL=="AC0", ATTR{online}=="1", RUN+="/bin/sh -c '${ac_command[@]}'"
-ACTION=="change", SUBSYSTEM=="power_supply", KERNEL=="AC0", ATTR{online}=="0", RUN+="/bin/sh -c '${battery_command[@]}'"
+ACTION=="change", SUBSYSTEM=="power_supply", KERNEL=="AC0", ATTR{online}=="1", RUN+="/bin/sh -c '${ac_commands[@]}'"
+ACTION=="change", SUBSYSTEM=="power_supply", KERNEL=="AC0", ATTR{online}=="0", RUN+="/bin/sh -c '${battery_commands[@]}'"
 EOL
 
 sudo udevadm control --reload-rules
